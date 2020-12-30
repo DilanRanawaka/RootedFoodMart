39092288cc3a0c02372fe0fa90ccd9e12729d1bd
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-12-30 20:51:40 +0530

    Create location.php

---
+++ location.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<!-- <html class="no-js" lang="zxx"> -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Location</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="manifest" href="site.html">
+    <link rel="apple-touch-icon" href="icon.html">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- bootstrap v4.0.0 -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- fontawesome-icons css -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <!-- themify-icons css -->
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <!-- elegant css -->
+    <link rel="stylesheet" href="assets/css/elegant.css">
+    <!-- meanmenu css -->
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <!-- animate css -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!-- venobox css -->
+    <link rel="stylesheet" href="assets/css/venobox.css">
+    <!-- jquery-ui.min css -->
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <!-- slick css -->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <!-- slick-theme css -->
+    <link rel="stylesheet" href="assets/css/slick-theme.css">
+    <!-- helper css -->
+    <link rel="stylesheet" href="assets/css/helper.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/index-style.css">
+    <!-- responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+
+    <!--header-area start-->
+    <header class="header-area">
+        <!--header-->
+        <div id="sticker" class="header-bottom">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-sm-2">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/images/logo.png" alt="logo"></a>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="mainmenu text-center">
+                            <nav>
+                                <ul>
+                                    <li><a href="index.html">Home</a>
+                                    </li>
+                                    <li><a href="#">Pages</a>
+                                    </li>
+                                    <li><a href="#">Services</a>
+                                    </li>
+                                    <li><a href="#">Gallery</a>
+                                    </li>
+                                    <li><a href="#">Blog</a>
+                                    </li>
+                                    <li><a href="#">Shop</a>
+                                    </li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--header-area end-->
+
+    <div class="page-banner-area bg-1">
+        <div class="container">
+            <div class="row align-items-center height-400">
+                <div class="col-lg-12">
+                    <div class="page-banner-text text-center mt-minus-10">
+                        <h2>Locate Farmers</h2>
+                        <ul class="site-breadcrumb text-black">
+                            <li><a href="#">Home</a> <span>></span></li>
+                            <li>Location <span>></span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="google-map-area mt-80 sm-mt-75 xs-mt-50">
+        <div id="googleMap" class="gmap-two"></div>
+    </div>
+    <!-- <div id="googleMap" style="width:100%;height:400px"></div> -->
+
+    <!-- <script src="https://maps.google.com/maps/api/js?sensor=false&amp;libraries=geometry&amp;v=3.22&amp;key=AIzaSyChs2QWiAhnzz0a4OEhzqCXwx_qA9ST_lE"></script> -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA9kJ-z44cnQ3QJHff1sQTlbnEYH-plu48&callback=myMap"></script>
+    <script>
+        google.maps.event.addDomListener(window, 'load', myMap);
+
+        function myMap() {
+            var mapProp = {
+                center: new google.maps.LatLng(6.9271, 79.8612),
+                scrollwheel: true,
+                zoom: 10,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(6.9271, 79.8612),
+                map: map
+
+
+            });
+
+            var infowindow = new google.maps.InfoWindow({
+                content: "Farmer 1"
+            });
+
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(6.0535, 80.2210),
+                map: map
+
+
+            });
+
+
+            var infowindow = new google.maps.InfoWindow({
+                content: "Farmer 1"
+            });
+
+            // google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+        }
+
+        
+    </script>
+
+    <!--footer-area start-->
+    <footer class="footer-area">
+        <!--footer-top-->
+        <div class="footer-widgets">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+                            <h4>Food Mart</h4>
+                            <div class="about-widget">
+                                <p>.................................................</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+                            <h4>Contact Info</h4>
+                            <div class="contact-info style-3">
+                                <ul>
+                                    <li><i class="fa fa-home"></i>Colombo, Sri Lanka</li>
+                                    <li><i class="fa fa-phone"></i>011-5490732</li>
+                                    <li><i class="fa fa-envelope"></i>keels@gmail.com</li>
+                                    <li><i class="fa fa-clock-o"></i>Monday - Sunday: 9.00AM - 9.00PM</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+                            <h4>Navigation</h4>
+                            <div class="fooer-menu">
+                                <ul class="mr-40">
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Service</a></li>
+                                    <li><a href="#">Gallery</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Shop</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+
+                            <div class="social-icons style-3 mt-35">
+                                <p>Follow us on</p>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--footer-area end-->
+
+    <!-- modernizr js -->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <!-- jquery-1.12.0 version -->
+    <script src="assets/js/vendor/jquery-1.12.0.min.js"></script>
+    <!-- bootstra.min js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- meanmenu js -->
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
+    <!-- easing js -->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!---venobox-js-->
+    <script src="assets/js/venobox.min.js"></script>
+    <!---slick-js-->
+    <script src="assets/js/slick.min.js"></script>
+    <!---waypoints-js-->
+    <script src="assets/js/waypoints.js"></script>
+    <!---counterup-js-->
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <!---isotop-js-->
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <!-- jquery-ui js -->
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <!-- jquery.countdown js -->
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    <!-- plugins js -->
+    <script src="assets/js/plugins.js"></script>
+    <!-- main js -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
