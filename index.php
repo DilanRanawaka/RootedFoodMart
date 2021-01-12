6cd63866766d67cd13b92f5a527cc3a78efce00d
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2021-01-12 14:36:51 +0530

    Update index.php

---
+++ index.php
@@ -0,0 +1,365 @@
+<!doctype html>
+<!-- <html class="no-js" lang="zxx"> -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>KEELS</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- bootstrap v4.0.0 -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- fontawesome-icons css -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <!-- themify-icons css -->
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <!-- elegant css -->
+    <link rel="stylesheet" href="assets/css/elegant.css">
+    <!-- meanmenu css -->
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <!-- animate css -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!-- venobox css -->
+    <link rel="stylesheet" href="assets/css/venobox.css">
+    <!-- jquery-ui.min css -->
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <!-- slick css -->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <!-- slick-theme css -->
+    <link rel="stylesheet" href="assets/css/slick-theme.css">
+    <!-- helper css -->
+    <link rel="stylesheet" href="assets/css/helper.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/index-style.css">
+    <!-- responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+
+<body>
+    <!--header-area start-->
+    <header class="header-area">
+        <div id="sticker" class="header-bottom">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-sm-2">
+                        <div class="logo">
+                            <h4><a href="index.html">ROOTED FOOT MART</a></h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="mainmenu text-center">
+                            <nav>
+                                <ul>
+                                    <li><a href="index.html">Home</a>
+                                    </li>
+                                    <li><a href="location.html">Mart</a>
+                                    </li>
+                                    <li><a href="harvest.html">Harvest</a>
+                                    </li>
+                                    <li><a href="contact.html">Place Order</a>
+                                    </li>
+                                    <li><a href="graphs.html">Insights</a>
+                                    </li>
+                                    <li><a href="#">Srvices</a>
+                                    </li>
+                                    <li><a href="">Contact Us</a>
+                                    </li>
+                                    <li><a href="login.html">Sign In</a>
+                                    </li>
+                                    <li><a href="register.html">Sign Up</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--header-area end-->
+
+    <!--slider-area start-->
+    <div class="slider-area">
+        <div class="container-fluid">
+            <div class="slider-items arrow-none">
+                <div class="single-slide bg-1">
+                    <div class="row height-800 align-items-center">
+                        <div class="col-lg-9 offset-lg-3 col-sm-12">
+                            <div class="banner-text style-3 text-black text-left mt-minus-58">
+                                <h2>Keels <br /> Rooted Food Mart</h2>
+                                <p class="mt-35">It is a long established fact that a reader will be distracted by the
+                                    readable <br /> content of a page when looking at its layout. The point of using Lorem
+                                    Ipsum</p>
+                                <a href="#" class="btn-common mt-40">View</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="single-slide bg-2 height-800">
+                    <div class="row height-800 align-items-center">
+                        <div class="col-sm-12">
+                            <div class="banner-text style-3 text-white text-center mt-minus-58">
+                                <h2> <br /> Rooted Food Mart</h2>
+                                <p class="mt-35">It is a long established fact that a reader will be distracted by the
+                                    readable <br /> content of a page when looking at its layout. The point of using Lorem
+                                    Ipsum</p>
+                                <a href="#" class="btn-common mt-40">View</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--slider-area end-->
+
+    <!--products-area start-->
+    <div class="products-area mt-65">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 offset-sm-2">
+                    <div class="section-title style-2 text-center">
+                        <h2>Products</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-product style-2">
+                        <div class="product-thumb-sin">
+                            <a href="#"><img src="assets/images/vegetables.jpg" alt="" /></a>
+                            <div class="product-action">
+                                <a href="#" class="add-to-cart">
+                                    <span>View</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <h4><a href="#">Vegitables</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-product style-2">
+                        <div class="product-thumb-sin">
+                            <a href="#"><img src="assets/images/2-2-2-3foodgroups_fruits_detailfeature_thumb.jpg" alt="" /></a>
+                            <div class="product-action">
+                                <a href="#" class="add-to-cart">
+                                    <span>View</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <h4><a href="#">Fruits</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-product style-2">
+                        <div class="product-thumb-sin">
+                            <a href="#"><img src="assets/images/whole-grain.jpg" alt="" /></a>
+                            <div class="product-action">
+                                <a href="#" class="add-to-cart">
+                                    <span>view</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <h4><a href="#">Grains</a></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="single-product style-2">
+                        <div class="product-thumb-sin">
+                            <a href="#"><img src="assets/images/how-to-use-celery-leaves-1.jpg" alt="" /></a>
+                            <div class="product-action">
+                                <a href="#" class="add-to-cart">
+                                    <span>View</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="product-text">
+                            <h4><a href="#">Leaves</a></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--products-area end-->
+
+
+    <div class="blog-area mt-62 sm-mt-58">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-8 offset-sm-2">
+                    <div class="section-title style-2">
+                        <h2>Farm</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-42">
+                <div class="col-lg-6 col-sm-12 single-blog style-3 style-4 p-0">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="blog-thumb">
+                                <a href="#"><img src="assets/images/9351_612790f47a1dcfe19a4b.jpg" alt="blog-image"></a>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 p-0">
+                            <div class="blog-desc text-left">
+                                <h3><a href="#">Farmer Locations</a></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip</p>
+                                <a href="#" class="btn-common">View</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 single-blog style-3 style-4 sm-mb-0 p-0">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="blog-thumb">
+                                <a href="#"><img src="assets/images/Harvest.jpg" alt="blog-image"></a>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 p-0">
+                            <div class="blog-desc text-left">
+                                <h3><a href="#">Harvest Information</a></h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip</p>
+                                <a href="#" class="btn-common">View</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!--contact-box-area start-->
+    <div class="contact-box-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="single-contact-box">
+                        <i class="fa fa-map-marker"></i>
+                        <h4>Our Location</h4>
+                        <p>Colombo, Sri Lanka</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="single-contact-box">
+                        <i class="fa fa-phone"></i>
+                        <h4>Phone Number</h4>
+                        <p>011-5490732</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 d-lg-block col-md-6 d-md-none col-sm-12">
+                    <div class="single-contact-box">
+                        <i class="fa fa-envelope"></i>
+                        <h4>Email Contact</h4>
+                        <p>Keels@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--contact-box-area end-->
+
+
+    <!--footer-area start-->
+    <footer class="footer-area">
+        <!--footer-top-->
+        <div class="footer-widgets">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+                            <h4>Keels Food Mart</h4>
+                            <div class="about-widget">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia reiciendis voluptatem nostrum, non,
+                                    quos repudiandae fuga iste dolorem delectus, </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+                            <h4>Contact Info</h4>
+                            <div class="contact-info style-3">
+                                <ul>
+                                    <li><i class="fa fa-home"></i>Colombo, Sri Lanka</li>
+                                    <li><i class="fa fa-phone"></i>011-5490732</li>
+                                    <li><i class="fa fa-envelope"></i>keels@gmail.com</li>
+                                    <li><i class="fa fa-clock-o"></i>Monday - Sunday: 9.00AM - 9.00PM</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+                            <h4>Navigation</h4>
+                            <div class="fooer-menu">
+                                <ul class="mr-40">
+                                    <li><a href="#">Mart</a></li>
+                                    <li><a href="#">Harvest</a></li>
+                                    <li><a href="#">Order</a></li>
+                                    <li><a href="#">Insights</a></li>
+                                    <li><a href="#">Services</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="footer-widget style-2">
+                            <div class="social-icons style-3 mt-35">
+                                <p>Follow us on</p>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--footer-area end-->
+
+    <!-- modernizr js -->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <!-- jquery-1.12.0 version -->
+    <script src="assets/js/vendor/jquery-1.12.0.min.js"></script>
+    <!-- bootstra.min js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- meanmenu js -->
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
+    <!-- easing js -->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!---venobox-js-->
+    <script src="assets/js/venobox.min.js"></script>
+    <!---slick-js-->
+    <script src="assets/js/slick.min.js"></script>
+    <!---waypoints-js-->
+    <script src="assets/js/waypoints.js"></script>
+    <!---counterup-js-->
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <!---isotop-js-->
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <!-- jquery-ui js -->
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <!-- jquery.countdown js -->
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    <!-- plugins js -->
+    <script src="assets/js/plugins.js"></script>
+    <!-- main js -->
+    <script src="assets/js/main.js"></script>
+
+</body>
