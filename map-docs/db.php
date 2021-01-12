e1220c6d05f87412771b100b77aa402b38d9dfcc
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-01-12 19:34:00 +0530

    Update db.php

--- map-docs/db.php
+++ map-docs/db.php
@@ -1,6 +1,6 @@
 <?php
 // Opens a connection to a MySQL server.
-$connection=mysqli_connect ("localhost", 'root', '','demo');
+$connection=mysqli_connect ("localhost", 'root', '','martinfodb');
 if (!$connection) {
     die('Not connected : ' . mysqli_connect_error());
 }
