c482e3f5fc3695af4bace44f16fed6dca6e798a2
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-01-17 00:13:14 +0530

    Update login.php

--- login.php
+++ login.php
@@ -85,10 +85,6 @@
                                     </li>
                                     <li><a href="graphs.php">Insights</a>
                                     </li>
-                                    <li><a href="#">Srvices</a>
-                                    </li>
-                                    <li><a href="">Contact Us</a>
-                                    </li>
                                     <li><a href="login.php">Sign In</a>
                                     </li>
                                     <li><a href="register.php">Sign Up</a>
