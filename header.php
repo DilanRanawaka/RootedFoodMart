710218e32d361f016c7674b45957792ebfbd48e3
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-01-17 19:49:32 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,70 @@
+   <!--header-area start-->
+   
+    <!-- bootstrap v4.0.0 -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- fontawesome-icons css -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <!-- themify-icons css -->
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <!-- elegant css -->
+    <link rel="stylesheet" href="assets/css/elegant.css">
+    <!-- meanmenu css -->
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <!-- animate css -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!-- venobox css -->
+    <link rel="stylesheet" href="assets/css/venobox.css">
+    <!-- jquery-ui.min css -->
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <!-- slick css -->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <!-- slick-theme css -->
+    <link rel="stylesheet" href="assets/css/slick-theme.css">
+    <!-- helper css -->
+    <link rel="stylesheet" href="assets/css/helper.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/index-style.css">
+    <!-- responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+   <header class="header-area">
+        <!--header-->
+        <div id="sticker" class="header-bottom">
+            <div class="container-fluid">
+                 <div class="row align-items-center">
+                    <div class="col-sm-2">
+                        <div class="logo"> 
+                            <h4><a href="index.html">ROOTED FOOT MART</a></h4>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="mainmenu text-center">
+                            <nav>
+                                <ul>
+                                <li><a href="index.php">Home</a>
+                                </li>
+                                <li><a href="location.php">Mart</a>
+                                </li>
+                                <li><a href="harvest.php">Harvest</a>
+                                </li>
+                                <li><a href="contact.php">Place Order</a>
+                                </li>
+                                <li><a href="graphs.php">Insights</a>
+                                </li>
+                                <li><a href="login.php">Sign In</a>
+                                </li>
+                                <li><a href="register.php">Sign Up</a>
+                                </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--header-area end-->
 <!DOCTYPE html>
 <html>
 <head>
